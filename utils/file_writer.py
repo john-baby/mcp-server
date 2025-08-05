76d69d6be0ea23906ee9649e45a3c9ba2b6d53fe
JOHN BABY <104748393+john-baby@users.noreply.github.com>
2025-08-05 14:20:39 +0530

    Create file_writer.py

---
+++ utils/file_writer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pathlib import Path
+from utils.file_reader import DATA_DIR
+
+def save_csv(df: pd.DataFrame, filename: str):
+    """
+    Save the DataFrame to a CSV file in the DATA_DIR directory.
+    Args:
+        df: The DataFrame to save.
+        filename: The name of the CSV file (e.g., 'attendance.csv')
+    """
+    file_path = DATA_DIR / filename
+    df.to_csv(file_path, index=False)
